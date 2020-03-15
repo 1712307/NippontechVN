067cc2aa96fe83b440209e490ee55f76dec4db74
lexuancuong <43180521+cuonglehcmus@users.noreply.github.com>
2020-03-15 16:03:01 +0700

    sadk

---
+++ public/js/script.js
@@ -0,0 +1,187 @@
+
+$('html').on('click','.img-click',function() {
+	console.log('click');
+    var select = $(this).attr('data-img');
+    $('.img-click').css("opacity","0.5");
+    $(this).css("opacity","1");
+    $('html').find('div.slides ul li').removeClass('hienlen');
+    $('html').find('div.slides ul li .ani-text-product h1').removeClass('style-ani-1');
+    $('html').find('div.slides ul li .move-text p').removeClass('style-text-ani');
+    $('html').find('div.slides ul li .bg-ani').removeClass('style-bg-ani');
+    $('html').find('div.slides ul li#' + select).addClass("hienlen");
+     $('html').find('div.slides ul li#' + select).find('.ani-text-product h1').addClass("style-ani-1");
+      $('html').find('div.slides ul li#' + select).find('.move-text p').addClass("style-text-ani");
+       $('html').find('div.slides ul li#' + select).find('.bg-ani').addClass("style-bg-ani");
+});
+
+$('html').on('click','.change-click .item',function() {
+	var change = $(this).attr('data-change');
+	$('html').find('.list-3 li').removeClass('show-img');
+	$('html').find('.list-3 li#' + change).addClass('show-img');
+});
+
+$('html').on('click','.color-click .item',function() {
+	var color = $(this).attr('data-color');
+	$('html').find('.slides2 ul li').removeClass('hienra');
+	$('html').find('.slides2 ul li#' + color).addClass('hienra');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+	/* SLIDE */
+	var list_img = document.querySelectorAll('.list-img li');
+	var bg_ani = document.querySelectorAll('.bg-ani');
+	var text_show = document.querySelectorAll('.move-text p');
+	var nut = document.querySelectorAll('div.nut ul li'),
+		nuttrai = nut[0],
+		nutphai = nut[1];
+	var slides = document.querySelectorAll('div.slides ul li');
+	var cuoicung = slides.length - 1;
+
+	var text_big = document.querySelectorAll('.ani-text-product h1');
+	chuyenslide = function() {
+		var vitrislideht = 0;
+		var slideht = document.querySelector('ul li.hienlen');
+		for(var i = 0; slideht = slideht.previousElementSibling; vitrislideht++){
+
+		}
+
+		for(var i = 0; i < slides.length; i++){
+			slides[i].classList.remove('hienlen');
+			text_big[i].classList.remove('style-ani-1');
+			text_show[i].classList.remove('style-text-ani');
+			bg_ani[i].classList.remove('style-bg-ani');
+
+		}
+		if(vitrislideht == 0){
+			vitrislideht = cuoicung + 1;
+			slides[vitrislideht-1].classList.add('hienlen');
+			text_show[vitrislideht-1].classList.add('style-text-ani');
+			bg_ani[vitrislideht-1].classList.add('style-bg-ani');
+			text_big[vitrislideht-1].classList.add('style-ani-1');
+			list_img[vitrislideht-1].style.opacity = '1';
+		}
+		else{
+		text_big[vitrislideht-1].classList.add('style-ani-1');
+		slides[vitrislideht-1].classList.add('hienlen');
+		text_show[vitrislideht-1].classList.add('style-text-ani');
+		bg_ani[vitrislideht-1].classList.add('style-bg-ani');
+		list_img[vitrislideht-1].style.opacity = '1';
+		}
+
+	}
+
+	chuyenslidep = function(){
+
+		var vitrislideht = 0;
+		var slideht = document.querySelector('ul li.hienlen');
+		for(var i = 0; slideht = slideht.previousElementSibling; vitrislideht++){
+
+		}
+
+		for(var i = 0; i < slides.length; i++){
+			slides[i].classList.remove('hienlen');
+			text_big[i].classList.remove('style-ani-1');
+			text_show[i].classList.remove('style-text-ani');
+			bg_ani[i].classList.remove('style-bg-ani');
+
+		}
+		if(vitrislideht == cuoicung){
+			vitrislideht = -1;
+
+		}
+
+		slides[vitrislideht+1].classList.add('hienlen');
+		text_big[vitrislideht+1].classList.add('style-ani-1');
+		text_show[vitrislideht+1].classList.add('style-text-ani');
+		bg_ani[vitrislideht+1].classList.add('style-bg-ani');
+		list_img[vitrislideht+1].style.opacity = '1';
+	}
+
+
+
+	/* AUTO - SLIDE */
+	  auto();
+    function auto(){
+    var thoigian = setInterval(function(){
+
+		var vitrislideht = 0;
+		var slideht = document.querySelector('ul li.hienlen');
+		for(var i = 0; slideht = slideht.previousElementSibling; vitrislideht++){
+
+		}
+		for(var i = 0; i < slides.length; i++){
+
+			slides[i].classList.remove('hienlen');
+			text_big[i].classList.remove('style-ani-1');
+			text_show[i].classList.remove('style-text-ani');
+			bg_ani[i].classList.remove('style-bg-ani');
+		}
+		if(vitrislideht == cuoicung){
+			vitrislideht = -1;
+
+		}
+
+		slides[vitrislideht+1].classList.add('hienlen');
+		text_big[vitrislideht+1].classList.add('style-ani-1');
+		text_show[vitrislideht+1].classList.add('style-text-ani');
+		bg_ani[vitrislideht+1].classList.add('style-bg-ani');
+		list_img[vitrislideht+1].style.opacity = '1';
+
+	},3000)
+// Tu dong chuyen slide
+	var imgClick = document.querySelectorAll('.img-click');
+    for(var i = 0; i < imgClick.length; i++){
+        nuttrai.addEventListener('click',function () {
+            clearInterval(thoigian);
+
+        })
+        nutphai.addEventListener('click',function () {
+            clearInterval(thoigian);
+
+        })
+        imgClick[i].addEventListener('click',function () {
+            clearInterval(thoigian);
+
+        })
+    }
+
+}
+
+	nuttrai.addEventListener('click',chuyenslide);
+	nutphai.addEventListener('click',chuyenslidep);
+                    /* AUTO - SLIDE - 2 */
+                      auto2();
+        var slides2 = document.querySelectorAll('div.slides2 ul li')
+        var cuoicung2 = slides2.length - 1;
+
+    function auto2(){
+    var thoigian2 = setInterval(function(){
+
+        var vitrislideht = 0;
+        var slideht2 = document.querySelector('ul li.hienra');
+        for(var i = 0; slideht2 = slideht2.previousElementSibling; vitrislideht++){
+
+        }
+        for(var i = 0; i < slides2.length; i++){
+            slides2[i].classList.remove('hienra');
+        }
+        if(vitrislideht == cuoicung2){
+            vitrislideht = -1;
+
+        }
+
+        slides2[vitrislideht+1].classList.add('hienra');
+
+    },3000)
+        var stop = document.querySelectorAll('.color-click .item');
+        for(var i = 0; i < stop.length; i++){
+        stop[i].addEventListener('click',function () {
+            clearInterval(thoigian2);
+
+        })
+        }
+    }
+
+},false);
